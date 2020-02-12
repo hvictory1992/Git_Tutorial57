76cad65521c3f03590af87362a600422440f0b7f
hvictory_1992 <victory_1992@naver.com>
2020-02-12 17:25:47 +0900

    Add User_Controller [Update]

--- User_Controller.java
+++ User_Controller.java
@@ -7,11 +7,19 @@ public class UserController {
 
     return "user/login";
   }
-
+  // 회원가입 페에지로 이동
   @RequestMapping("user/insert.do")
   public String insert() {
     logger.info("경로 : insert")
 
     return "user/insert";
+  }
+
+  // 회원 수정페이지로 이동
+  @RequestMapping("user/update.do")
+  public String update() {
+    logger.info("경로 : update")
+
+    return "user/update";
   }
 }
